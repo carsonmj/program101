d37c5b0dfe1602954bc29bd5bbf17bfbf36470ce
minji <eunoia2040@gmail.com>
2022-03-02 03:17:06 +0900

    Design(theme): update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -10,10 +10,18 @@ const fontSizes = {
   xxl: calcRem(1.8),
 };
 
-const fontWeight = {
+const fontWeights = {
   light: 200,
   normal: 400,
   bold: 700,
+};
+
+const iconSize = {
+  xs: calcRem(1.5),
+  sm: calcRem(2.0),
+  md: calcRem(2.5),
+  lg: calcRem(3.5),
+  xl: calcRem(5),
 };
 
 const space = {
@@ -25,18 +33,19 @@ const space = {
 };
 
 const colors = {
-  black_1: "18181C",
-  black_2: "433B3B",
-  darkblue_1: "201D47",
-  darkblue_2: "2E2B52",
-  grayblue_1: "585858",
-  gray_1: "6F6C99",
-  blue_1: "0078FE",
-  lightblue_1: "14B7E5",
-  lightblue_2: "53B9EA",
-  green_1: "3AC090",
-  yellow_1: "F5A623",
-  white_1: "FFFFFF",
+  black_1: "#18181C",
+  black_2: "#433B3B",
+  darkblue_1: "#201D47",
+  darkblue_2: "#2E2B52",
+  grayblue_1: "#585858",
+  gray_1: "#6F6C99",
+  gray_2: "#585858",
+  blue_1: "#0078FE",
+  lightblue_1: "#14B7E5",
+  lightblue_2: "#53B9EA",
+  green_1: "#3AC090",
+  yellow_1: "#F5A623",
+  white_1: "#FFFFFF",
 };
 
 const linearColors = {
@@ -51,6 +60,6 @@ const opacityColors = {
   yellow_1: "FCE5AB",
 };
 
-const theme = { fontSizes, fontWeight, space, colors, linearColors, opacityColors };
+const theme = { fontSizes, fontWeights, space, colors, linearColors, opacityColors, iconSize };
 
 export default theme;
