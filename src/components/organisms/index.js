42d2a6eec3ef0d3ece759cc412f2ff011e2519f6
minji <eunoia2040@gmail.com>
2022-03-04 12:02:52 +0900

    update(organisms): add DefaultModal

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,7 +1,8 @@
 import CodeEditor from "./CodeEditor";
+import DefaultModal from "./DefaultModal";
 import FileTree from "./FileTree";
 import GreetingModal from "./GreetingModal";
 import Header from "./Header";
 import StepProgressBar from "./StepProgressBar";
 
-export { CodeEditor, FileTree, GreetingModal, Header, StepProgressBar };
+export { CodeEditor, DefaultModal, FileTree, GreetingModal, Header, StepProgressBar };
