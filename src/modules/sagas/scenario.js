6d71f16ec929b0a8d85ad3d141f108244b028049
minji <eunoia2040@gmail.com>
2022-03-04 11:37:27 +0900

    feat(scenarioSaga): add scenario saga for data fetching

---
+++ src/modules/sagas/scenario.js
@@ -0,0 +1,20 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+
+import { getScenariosAPI, getFilesAPI } from "../../apis";
+import { scenarioSliceActions } from "../slices/scenarioSlice";
+
+function* getScenariosSaga() {
+  const { getScenariosSuccess, getScenariosFailure } = scenarioSliceActions;
+
+  try {
+    const scenarios = yield call(getScenariosAPI);
+
+    yield put(getScenariosSuccess(scenarios));
+  } catch (err) {
+    yield put(getScenariosFailure(err));
+  }
+}
+
+export function* watchGetScenarios() {
+  yield takeEvery(scenarioSliceActions.getScenarios, getScenariosSaga);
+}
