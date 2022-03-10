11dc94f5bd8438cef73076583a9329e544b76d8a
minji <eunoia2040@gmail.com>
2022-03-11 00:21:12 +0900

    feat(visualization): update using prop for animation, add animation

--- src/components/organisms/Visualization/index.js
+++ src/components/organisms/Visualization/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { debounce } from "lodash";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,7 +7,7 @@ import styled from "styled-components";
 import { LightCircle } from "../../atoms";
 import { scenarioSliceActions } from "../../../modules/slices/scenarioSlice";
 
-const Visualization = (props) => {
+const Visualization = () => {
   const dispatch = useDispatch();
   const scenarios = useSelector((state) => state.scenario.scenarios);
   const current = useSelector((state) => state.scenario.current);
@@ -19,7 +19,7 @@ const Visualization = (props) => {
       dispatch(scenarioSliceActions.updateCurrentScenario(nextId));
       dispatch(scenarioSliceActions.updateCurrent(scenarios[nextId]));
     }
-  }, 2000);
+  }, 1000);
 
   useEffect(() => {
     if (current?.type === "visualize" && current?.action) {
@@ -30,69 +30,40 @@ const Visualization = (props) => {
   return (
     <Container onAnimationEnd={handleAnimationEnd} actions={actions}>
       <Wrapper>
-        <StoreBox
-          actions={actions}
-          className={current?.type === "visualize" && current?.action === "showStore" && "fadeIn"}
-        >
+        <StoreBox actions={actions} animation={current?.action}>
           <StoreLeftSide>
-            <Dispatch
-              actions={actions}
-              className={current?.type === "visualize" && current?.action === "showStore" && "fadeIn"}
-            >
+            <Dispatch actions={actions} animation={current?.action}>
               Dispatch
             </Dispatch>
             <Text align="left">Store</Text>
           </StoreLeftSide>
           <StoreRightSide>
-            <ReducerBox
-              actions={actions}
-              className={current?.type === "visualize" && current?.action === "showReducerAndState" && "fadeIn"}
-            >
+            <ReducerBox actions={actions} animation={current?.action}>
               <Text align="center">Reducer</Text>
-              <BoxWrapper
-                actions={actions}
-                className={current?.type === "visualize" && current?.action === "showReducers" && "fadeIn"}
-              >
+              <BoxWrapper actions={actions} animation={current?.action}>
                 <InnerBox>increment</InnerBox>
-                <InnerBox>decrement</InnerBox>
+                <InnerBox animation={current?.action}>decrement</InnerBox>
               </BoxWrapper>
             </ReducerBox>
-            <StateBox
-              actions={actions}
-              className={current?.type === "visualize" && current?.action === "showReducerAndState" && "fadeIn"}
-            >
+            <StateBox actions={actions} animation={current?.action}>
               <Text align="center">State</Text>
-              <BoxWrapper
-                actions={actions}
-                className={current?.type === "visualize" && current?.action === "showReducerAndState" && "fadeIn"}
-              >
-                <InnerBox>count: 0</InnerBox>
+              <BoxWrapper actions={actions} animation={current?.action}>
+                <InnerBox>count: 1</InnerBox>
               </BoxWrapper>
             </StateBox>
           </StoreRightSide>
         </StoreBox>
-        <ViewBox
-          actions={actions}
-          className={current?.type === "visualize" && current?.action === "showViewComponent" && "fadeIn"}
-        >
-          <DispatchActionBox>{`dispatch({ type: increment })`}</DispatchActionBox>
-          <CounterWrapper
-            actions={actions}
-            className={current?.type === "visualize" && current?.action === "showViewComponent" && "fadeIn"}
-          >
-            <MinusButton>-</MinusButton>
-            <CounterText>0</CounterText>
+        <ViewBox actions={actions} animation={current?.action}>
+          <DispatchActionBox animation={current?.action}>{"dispatch({ type: counter/decrement })"}</DispatchActionBox>
+          <CounterWrapper actions={actions} animation={current?.action}>
+            <MinusButton animation={current?.action}>-</MinusButton>
+            <CounterText>1</CounterText>
             <PlusButton>+</PlusButton>
           </CounterWrapper>
           <Text color="dark">View</Text>
         </ViewBox>
-        <PathBox
-          actions={actions}
-          className={current?.type === "visualize" && current?.action === "showPath" && "fadeInPath"}
-        >
-          <Circle
-            className={current?.type === "visualize" && current?.action === "moveStateToView" && "moveStateToView"}
-          >
+        <PathBox actions={actions} animation={current?.action}>
+          <Circle animation={current?.action}>
             <LightCircle />
           </Circle>
         </PathBox>
@@ -111,22 +82,6 @@ const Container = styled.div`
   height: 61rem;
   margin-top: 3rem;
   color: ${({ theme }) => theme.colors.white_1};
-
-  .fadeIn {
-    animation: fadeIn 0.5s linear 1;
-    animation-fill-mode: forwards;
-  }
-
-  .fadeInPath {
-    animation: fadeInPath 3s linear 1;
-    animation-fill-mode: forwards;
-  }
-
-  .moveStateToView {
-    visibility: visible;
-    animation: moveStateToView 3s linear 1;
-    animation-fill-mode: forwards;
-  }
 
   @keyframes fadeInPath {
     0% {
@@ -181,6 +136,62 @@ const Container = styled.div`
       left: 470px;
     }
   }
+
+  @keyframes moveViewToDispatch {
+    0% {
+      top: 255px;
+      left: 40px;
+    }
+    20% {
+      top: 255px;
+      left: -25px;
+    }
+    80% {
+      top: -25px;
+      left: -25px;
+    }
+    100% {
+      top: -25px;
+      left: 35px;
+    }
+  }
+
+  @keyframes blink {
+    0% {
+      box-shadow: 1px 1px 2px gray, 0 0 25px white, 0 0 5px yellow;
+      // box-shadow: 1px 1px 2px gray, 0 0 25px white, 0 0 5px darkblue;
+    }
+    100% {
+      background: #f1e1c2c9;
+      // background: #7a71db;
+    }
+  }
+
+  @keyframes blink_white {
+    0% {
+      box-shadow: 1px 1px 2px gray, 0 0 25px white, 0 0 5px pink;
+    }
+    100% {
+      background: #d5cdcd;
+    }
+  }
+
+  @keyframes showDispatchAction {
+    0% {
+      visibility: visible;
+      opacity: 0;
+      box-shadow: 1px 1px 2px gray, 0 0 25px white, 0 0 5px yellow;
+    }
+    50% {
+      visibility: visible;
+      opacity: 1;
+      box-shadow: 1px 1px 2px gray, 0 0 25px white, 0 0 5px yellow;
+    }
+    100% {
+      visibility: hidden;
+      opacity: 0;
+    }
+  }
 `;
 
 const Wrapper = styled.div`
@@ -197,9 +208,19 @@ const StoreBox = styled.div`
   height: 32rem;
   padding-left: ${({ theme }) => theme.space.xl};
   visibility: ${({ actions }) => (actions.includes("showStore") ? "visible" : "hidden")};
+  // visibility: visible;
   background: ${({ theme }) => theme.opacityColors.grayblue_1};
   border-radius: 1rem;
   z-index: 2;
+
+  ${({ animation }) => {
+    if (animation === "showStore") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const ViewBox = styled.div`
@@ -211,10 +232,20 @@ const ViewBox = styled.div`
   height: 22rem;
   padding-left: ${({ theme }) => theme.space.xl};
   visibility: ${({ actions }) => (actions && actions.includes("showViewComponent") ? "visible" : "hidden")};
+  // visibility: visible;
   background: ${({ theme }) => theme.colors.white_1};
   border-radius: 1rem;
   margin-top: 4rem;
   z-index: 2;
+
+  ${({ animation }) => {
+    if (animation === "showViewComponent") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const StoreLeftSide = styled.div`
@@ -239,23 +270,43 @@ const StoreRightSide = styled.div`
 const ReducerBox = styled.div`
   width: 24rem;
   height: 14rem;
-  visibility: hidden;
   visibility: ${({ actions }) => (actions.includes("showReducerAndState") ? "visible" : "hidden")};
+  // visibility: visible;
   background: ${({ theme }) => theme.opacityColors.pink_1};
   border-radius: 1rem;
   z-index: 2;
-  animation: rotateBorder 1s linear 1;
+
+  ${({ animation }) => {
+    if (animation === "showReducerAndState") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+
+    if (animation === "moveToReducer") {
+      return "animation: blink_white 0.8s linear 1;";
+    }
+  }}
 `;
 
 const StateBox = styled.div`
   width: 24rem;
   height: 14rem;
-  visibility: hidden;
   visibility: ${({ actions }) => (actions.includes("showReducerAndState") ? "visible" : "hidden")};
+  // visibility: visible;
   background: ${({ theme }) => theme.opacityColors.yellow_1};
   border-radius: 1rem;
   z-index: 2;
-  animation: rotateBorder 1s linear 1;
+
+  ${({ animation }) => {
+    if (animation === "showReducerAndState") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const Dispatch = styled.div`
@@ -267,12 +318,27 @@ const Dispatch = styled.div`
   line-height: 9rem;
   border-radius: 1rem;
   visibility: ${({ actions }) => (actions.includes("showStore") ? "visible" : "hidden")};
+  // visibility: visible;
+
   background: ${({ theme }) => theme.opacityColors.deepblue_1};
   color: ${({ theme }) => theme.colors.white_1};
   font-size: ${({ theme }) => theme.fontSizes.head6};
   font-weight: ${({ theme }) => theme.fontWeights.mid};
   text-align: center;
   z-index: 2;
+
+  ${({ animation }) => {
+    if (animation === "showStore") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+
+    if (animation === "blinkDispatch") {
+      return "animation: blink 1s linear 2";
+    }
+  }}
 `;
 
 const PathBox = styled.div`
@@ -282,11 +348,21 @@ const PathBox = styled.div`
   width: 60rem;
   height: 28rem;
   visibility: ${({ actions }) => (actions.includes("showPath") ? "visible" : "hidden")};
+  // visibility: visible;
   background: transparent;
   // border: 0.2rem dashed transparent;
   border: 0.1rem solid ${({ theme }) => theme.colors.grayblue_1};
   border-radius: 0.4rem;
   z-index: 0;
+
+  ${({ animation }) => {
+    if (animation === "showPath") {
+      return `
+      animation: fadeInPath 3s linear 1;
+      animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const Circle = styled.div`
@@ -294,7 +370,26 @@ const Circle = styled.div`
   top: 0;
   left: 0;
   visibility: hidden;
+  // visibility: visible;
   z-index: 1;
+
+  ${({ animation }) => {
+    if (animation === "moveStateToView") {
+      return `
+        visibility: visible;
+        animation: moveStateToView 3s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+
+    if (animation === "moveViewToDispatch") {
+      return `
+        visibility: visible;
+        animation: moveViewToDispatch 3s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const BoxWrapper = styled.div`
@@ -302,17 +397,16 @@ const BoxWrapper = styled.div`
   justify-content: space-around;
   width: 100%;
   visibility: ${({ actions }) => (actions && actions.includes("showReducers") ? "visible" : "hidden")};
-`;
+  // visibility: visible;
 
-const InnerBox = styled.div`
-  width: 10rem;
-  height: 5rem;
-  line-height: 5rem;
-  border-radius: 1rem;
-  background: ${({ theme }) => theme.opacityColors.white_1};
-  color: ${({ theme }) => theme.colors.gray_3};
-  text-align: center;
-  animation: rotateBorder 1s linear 1;
+  ${({ animation }) => {
+    if (animation === "showReducers") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+  }}
 `;
 
 const Text = styled.div`
@@ -333,9 +427,23 @@ const CounterWrapper = styled.div`
   width: 18rem;
   height: 4rem;
   visibility: ${({ actions }) => (actions && actions.includes("showViewComponent") ? "visible" : "hidden")};
+  // visibility: visible;
   boder-radius: 1rem;
   border-radius: 1rem;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+
+  ${({ animation }) => {
+    if (animation === "showViewComponent") {
+      return `
+        animation: fadeIn 0.5s linear 1;
+        animation-fill-mode: forwards;
+      `;
+    }
+
+    if (animation === "blinkCounter") {
+      return "animation: blink 1s linear 2;";
+    }
+  }}
 `;
 
 const MinusButton = styled.div`
@@ -346,6 +454,14 @@ const MinusButton = styled.div`
   background: ${({ theme }) => theme.colors.darkblue_1};
   font-size: ${({ theme }) => theme.fontSizes.head2};
   border-radius: 1rem 0rem 0rem 1rem;
+
+  ${({ animation }) => {
+    if (animation === "showClickEvent") {
+      return `
+        animation: blink 1s linear 2;
+      `;
+    }
+  }}
 `;
 
 const PlusButton = styled.div`
@@ -369,11 +485,40 @@ const CounterText = styled.div`
 `;
 
 const DispatchActionBox = styled.div`
-  width: 24rem;
+  width: 28rem;
   height: 3.5rem;
   line-height: 3.5rem;
   visibility: ${({ actions }) => (actions && actions.includes("showViewComponent") ? "visible" : "hidden")};
+  visibility: hidden;
+  margin-bottom: 4rem;
   border-radius: 2rem;
   background: ${({ theme }) => theme.colors.grayblue_1};
+  background: #3d4c70;
   text-align: center;
+
+  ${({ animation }) => {
+    if (animation === "dispatchCall") {
+      return `
+        animation: showDispatchAction 2s linear 2;
+      `;
+    }
+  }}
+`;
+
+const InnerBox = styled.div`
+  width: 10rem;
+  height: 5rem;
+  line-height: 5rem;
+  border-radius: 1rem;
+  background: ${({ theme }) => theme.opacityColors.white_1};
+  color: ${({ theme }) => theme.colors.gray_3};
+  text-align: center;
+
+  ${({ animation }) => {
+    if (animation === "moveToDecrement") {
+      return `
+        animation: blink 1s linear 2;
+      `;
+    }
+  }}
 `;
