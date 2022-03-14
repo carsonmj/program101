17882f951ad808fb474bb734f124b7992e36cce7
minji <eunoia2040@gmail.com>
2022-03-14 19:20:31 +0900

    feat(CodeEditor): add logics for calculate modal coordinates

--- src/components/organisms/CodeEditor/index.js
+++ src/components/organisms/CodeEditor/index.js
@@ -3,13 +3,14 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
-import { fileSliceActions } from "../../../modules/slices/fileSlice";
 import { getCodeFileAPI } from "../../../apis";
+import { fileSliceActions } from "../../../modules/slices/fileSlice";
 
 const CodeEditor = () => {
   const dispatch = useDispatch();
   const currentFile = useSelector((state) => state.file.currentFile);
   const highlightLine = useSelector((state) => state.file.highlightLines);
+  const modalDirection = useSelector((state) => state.scenario.current?.modalDirection);
   const [codes, setCodes] = useState(null);
   const [codeLines, setCodeLines] = useState(null);
 
@@ -26,15 +27,23 @@ const CodeEditor = () => {
     if (currentFile) {
       codes && setCodeLines(codes[currentFile].code.split("\n"));
     }
-  }, [currentFile]);
+  }, [currentFile, setCodeLines]);
 
   useEffect(() => {
-    if (highlightLine && codeLines && highlightLine.length > 0) {
+    if (highlightLine && codeLines && modalDirection && highlightLine.length > 0) {
+      if (modalDirection === "right") {
+        const $li = document.querySelector(`#line1`);
+        const rect = $li.getBoundingClientRect();
+
+        dispatch(fileSliceActions.setModalCoordinate({ top: rect.y + rect.bottom, left: rect.x - 320 }));
+        return;
+      }
+
       const lastLine = highlightLine === "all" ? codeLines.length : highlightLine[highlightLine.length - 1];
       const $li = document.querySelector(`#line${lastLine}`);
       const rect = $li.getBoundingClientRect();
 
-      dispatch(fileSliceActions.setModalCoordinate({ top: rect.top + 20, left: rect.left + 50 }));
+      dispatch(fileSliceActions.setModalCoordinate({ top: rect.top + 30, left: rect.left + 50 }));
     }
   }, [highlightLine, codeLines]);
 
