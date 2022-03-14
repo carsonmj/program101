ba71c4f64db14a111a7ed608d407d90570a4e6c3
minji <eunoia2040@gmail.com>
2022-03-14 19:18:32 +0900

    feat(DivideModal): add DivideModal component

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,9 +1,23 @@
+import Answers from "./Answers";
 import CodeEditor from "./CodeEditor";
 import DefaultModal from "./DefaultModal";
+import DivideModal from "./DivideModal";
 import FileTree from "./FileTree";
 import GreetingModal from "./GreetingModal";
 import Header from "./Header";
-import Visualization from "./Visualization";
+import PracticeCodeEditor from "./PracticeCodeEditor";
 import StepProgressBar from "./StepProgressBar";
+import Visualization from "./Visualization";
 
-export { CodeEditor, DefaultModal, FileTree, GreetingModal, Header, Visualization, StepProgressBar };
+export {
+  Answers,
+  CodeEditor,
+  DivideModal,
+  DefaultModal,
+  FileTree,
+  GreetingModal,
+  Header,
+  PracticeCodeEditor,
+  StepProgressBar,
+  Visualization,
+};
